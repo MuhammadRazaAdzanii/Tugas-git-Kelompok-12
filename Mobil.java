953cc81dadf16b147f2fda7f24f8ae2a92ab4c0b
Fikri <131033741+MaulanaFikriii@users.noreply.github.com>
2023-11-10 22:45:01 +0700

    Create Mobil.java

---
+++ Mobil.java
@@ -0,0 +1,21 @@
+public class Mobil extends KendaraanDarat {
+   
+   
+    
+    public Mobil(String Jenis, String Nama, int Tahun, String Warna,  double Jarak, double Waktu) {
+        super(Jenis, Nama, Tahun, Warna, Jarak, Waktu );
+       
+    }
+    
+    void BahanBakar()
+    {
+        
+    }
+    
+    public void JenisKendaraan(){
+        this.setJenis("civic");
+    }
+    
+       
+    
+}
