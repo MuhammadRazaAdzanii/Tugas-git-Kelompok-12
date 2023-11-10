ffadd7c91a08eb41def15498953bc4e3b9fea32b
Fikri <131033741+MaulanaFikriii@users.noreply.github.com>
2023-11-10 22:46:36 +0700

    Create Motor.java

---
+++ Motor.java
@@ -0,0 +1,15 @@
+public class Motor extends KendaraanDarat {
+   
+    
+    public Motor(String Jenis, String Nama, int Tahun, String Warna,  double Jarak, double Waktu) {
+        super(Jenis, Nama, Tahun, Warna, Jarak, Waktu );
+        
+    }
+
+    public void JenisKendaraan(){
+        this.setJenis("civic");
+    }
+    
+       
+    
+}
