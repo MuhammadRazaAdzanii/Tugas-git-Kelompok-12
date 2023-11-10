b8971da239ebd2a54ad7a8b670ddc1822ed04e32
Fikri <131033741+MaulanaFikriii@users.noreply.github.com>
2023-11-10 22:46:13 +0700

    Create MobilListrik.java

---
+++ MobilListrik.java
@@ -0,0 +1,15 @@
+public class MobilListrik extends Mobil {
+   
+
+    
+    public MobilListrik(String Jenis, String Nama, int Tahun, String Warna, double Jarak, double Waktu) {
+        super(Jenis, Nama, Tahun, Warna, Jarak, Waktu );
+      
+        
+    }
+       
+    void BahanBakar()
+    {
+        System.out.println("Listrik");
+    }
+}
