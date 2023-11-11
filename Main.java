c1336bb7b833a0d767caee6144522c39e123624e
Muhammad Raza Adzani <131844692+MuhammadRazaAdzanii@users.noreply.github.com>
2023-11-11 12:28:10 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,22 +2,22 @@
 public class Main
 {
     public static void main(String[] args) {
-        Mobil mobil1 = new Mobil("Mobil","Corolla", 2022, "Red",0.3,0.9);
+        Mobil mobil1 = new Mobil("Mobil","BMW M4", 2022, "Red",0.3,0.9);
         mobil1.display();
         
-        Motor motor1 = new Motor("Mobil","Corolla", 2022, "Red",0.2,0.2);
+        Motor motor1 = new Motor("Motor","ZX25r", 2021, "Green",0.2,0.2);
         motor1.display();
         
-        Bus bus1 = new Bus("Mobil","Corolla", 2022, "Red",0.8,0.2);
+        Bus bus1 = new Bus("Bus","Pelangi", 2023, "Rinbow",0.8,0.2);
         bus1.display();
         
-        Becak becak1 = new Becak("Mobil","Corolla", 2022, "Red",0.2,0.5);
+        Becak becak1 = new Becak("Becak","Becak Jawa", 2019, "Red",0.2,0.5);
         becak1.display();
         
-        Mobil a = new MobilListrik("civic", "bobon", 2004, "Merah",0.3,0.4);
+        Mobil a = new MobilListrik("mobil", "Tesla", 2023, "Black",0.3,0.4);
         a.BahanBakar();
         
-        Mobil b = new MobilBensin("civic", "bobon", 2004, "Merah",0.3,0.5);
+        Mobil b = new MobilBensin("Mobil", "civic", 2018, "White",0.3,0.5);
         b.BahanBakar();
     }
     
