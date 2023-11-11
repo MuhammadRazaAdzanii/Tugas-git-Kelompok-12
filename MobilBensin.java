dbe57dcd7680f6856a36c5501f5cc6babb8f2c26
Muhammad Raza Adzani <131844692+MuhammadRazaAdzanii@users.noreply.github.com>
2023-11-11 12:29:02 +0700

    Update MobilBensin.java

--- MobilBensin.java
+++ MobilBensin.java
@@ -1,11 +1,7 @@
 public class MobilBensin extends Mobil {
-  
-    
     public MobilBensin(String Jenis, String Nama, int Tahun, String Warna, double Jarak, double Waktu) {
         super(Jenis, Nama, Tahun, Warna, Jarak, Waktu );
-        
     }
-    
     void BahanBakar()
     {
         System.out.println("Bensin");
