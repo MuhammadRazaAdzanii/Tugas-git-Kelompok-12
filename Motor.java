7fc94807665c1b0926e98e1d86beba47a1df5961
Muhammad Raza Adzani <131844692+MuhammadRazaAdzanii@users.noreply.github.com>
2023-11-11 12:31:19 +0700

    Update Motor.java

--- Motor.java
+++ Motor.java
@@ -1,15 +1,9 @@
 public class Motor extends KendaraanDarat {
-   
-    
     public Motor(String Jenis, String Nama, int Tahun, String Warna,  double Jarak, double Waktu) {
-        super(Jenis, Nama, Tahun, Warna, Jarak, Waktu );
-        
+        super(Jenis, Nama, Tahun, Warna, Jarak, Waktu )        
     }
 
     public void JenisKendaraan(){
         this.setJenis("civic");
     }
-    
-       
-    
 }
