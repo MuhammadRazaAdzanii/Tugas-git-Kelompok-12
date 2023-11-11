28a2972c567b7774ca4cfb4e2d844e05401db05b
Muhammad Raza Adzani <131844692+MuhammadRazaAdzanii@users.noreply.github.com>
2023-11-11 12:32:36 +0700

    Update Becak.java

--- Becak.java
+++ Becak.java
@@ -1,9 +1,7 @@
-public class Becak extends KendaraanDarat
- {
+public class Becak extends KendaraanDaran{
    
     public Becak(String Jenis, String Nama, int Tahun, String Warna, double Jarak, double Waktu) {
         super(Jenis, Nama, Tahun, Warna,  Jarak, Waktu );
-        
     }
 
     public void JenisKendaraan(){
