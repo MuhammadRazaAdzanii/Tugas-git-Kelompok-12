7836feee840707f90d869f53b405ff514a4cee8a
Muhammad Raza Adzani <131844692+MuhammadRazaAdzanii@users.noreply.github.com>
2023-11-11 12:29:19 +0700

    Update MobilListrik.java

--- MobilListrik.java
+++ MobilListrik.java
@@ -1,13 +1,8 @@
 public class MobilListrik extends Mobil {
    
-
-    
     public MobilListrik(String Jenis, String Nama, int Tahun, String Warna, double Jarak, double Waktu) {
-        super(Jenis, Nama, Tahun, Warna, Jarak, Waktu );
-      
-        
+        super(Jenis, Nama, Tahun, Warna, Jarak, Waktu );     
     }
-       
     void BahanBakar()
     {
         System.out.println("Listrik");
