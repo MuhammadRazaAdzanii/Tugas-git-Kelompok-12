e848066b82cdbb69a5cd8ef99ad371569798e711
Muhammad Raza Adzani <131844692+MuhammadRazaAdzanii@users.noreply.github.com>
2023-11-11 12:31:55 +0700

    Update Mobil.java

--- Mobil.java
+++ Mobil.java
@@ -2,12 +2,10 @@ public class Mobil extends KendaraanDarat {
     
     public Mobil(String Jenis, String Nama, int Tahun, String Warna,  double Jarak, double Waktu) {
         super(Jenis, Nama, Tahun, Warna, Jarak, Waktu );
-       
     }
+    
     void BahanBakar()
-    {
-        
-    }
+    { }
     
     public void JenisKendaraan(){
         this.setJenis("civic");
